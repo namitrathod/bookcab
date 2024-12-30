d6f2557ba0b8500490a9cda83ce265301104ae34
namitrathod <namitrathod07@gmail.com>
2024-12-30 16:13:29 -0600

    register user backend

---
+++ backend/model/user.model.js
@@ -0,0 +1,48 @@
+const mongoose =require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { use } = require('../app')
+
+
+const userSchema= new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type: String,
+            required: true,
+            minlength:[3,'first name should be atleast 3 characher long'],
+        },
+        lastname:{
+            type: String,
+            required: true,
+            minlength:[3,'Last name should be atleast 3 characher long'],
+        },
+    },
+    email:{
+        type: String,
+        required: true,
+        minlength:[3,'email should be atleast 3 characher long'],
+    },
+    password:{
+        type:String,
+        required: true,
+        select: false
+    },
+    socketId: {
+        type:String,
+    }
+})
+
+userSchema.methods.generateAuthToken = function(){
+    const token =jwt.sign({_id:this._id}, process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.method.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+userSchema.statics.hashPassword = async function (password){
+    return await bcrypt.hash(password,10);
+}
+
+const userModel = mongoose.model('User',userSchema)
+module.exports= userModel
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { ExpressValidator } = require('express-validator');
+const {body} = require('express-validator')
+const userController = require('../controllers/user.controller')
+const router = express.Router();
+
+router.post('/register',[
+    body('email').isEmail().withMessage('Invalid email'),
+    body('fullname.firstname').isLength({min:3}).withMessage('first letter at least 3 character long'),
+    body('password').isLength({min:6}).withMessage('password must be 6 character long'),
+],
+userController.registerUser
+)
+
+
+module.exports = router;
